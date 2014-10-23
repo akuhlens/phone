b4ee21ed1a9e4056cb6039d3a020d24c629e40a8
Andre Kuhlenschmidt <akuhlens@indiana.edu>
2014-10-23 13:55:53 -0400

    First commit

---
+++ Phone_Interface/Button.h
@@ -0,0 +1,40 @@
+#ifndef Button_h
+#define Button_h
+#include "mbed.h"
+#include "UserInterface.h"
+#include "SeeedStudioTFTv2.h"
+#include "Command.h"
+
+class Button: public Touchable, public Drawable{
+    protected:
+    // these two is very hacky and needs removed
+      Command          *controller; 
+      SeeedStudioTFTv2 *display; 
+      int x, y, width, height, bordorWidth, bgColor, fgColor;
+      const char* label;
+
+    public:
+      virtual void touch(point& p) = 0;
+      virtual void draw();
+      Button(int x, int y, int width, int height, const char* label, Command *ctl, SeeedStudioTFTv2 *display);
+};
+
+class CharButton: public Button {
+    private:
+     char value;
+     const char* subValue;
+    public:
+     virtual void touch(point& p);
+     virtual void draw();
+     CharButton(int x, int y, int width, int height, const char* label, char value, const char *subValue, Command *ctl, SeeedStudioTFTv2 *display);
+};
+
+class CommandButton: public Button {
+    private:
+      int command;
+    public:
+      virtual void touch(point& p);
+      virtual void draw();
+      CommandButton(int x, int y, int width, int height, int mode, const char* label, Command *ctl, SeeedStudioTFTv2 *display);
+};
+#endif
