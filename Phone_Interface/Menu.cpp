ba73b11052b6ae8b66e1491b4da63eb43f7011ba
Andre Kuhlenschmidt <akuhlens@indiana.edu>
2014-10-23 16:38:24 -0400

    Testing actions

--- Phone_Interface/Menu.cpp
+++ Phone_Interface/Menu.cpp
@@ -1,16 +1,18 @@
 #include "Menu.h"
 #include "Button.h"
 
-Menu::Menu(Command *command, SeeedStudioTFTv2 *display): UserInterface(3 , 3, 0){
+Menu::Menu(Command *command, SeeedStudioTFTv2 *display): UserInterface(1 , 1, 0){
     // Initialize the three buttons on the menu screen
-    CommandButton *dialer   = new CommandButton(0,   0, 240, 80, GOTO_DIALER,    "Make Call", command, display);
-    CommandButton *texter   = new CommandButton(0,  80, 240, 80, GOTO_TEXTER,    "Send Text", command, display);
-    CommandButton *viewText = new CommandButton(0, 160, 240, 80, GOTO_VIEW_TEXT, "View Text", command, display);
-    // Register them as both drawlable and touchable
+    
+    Action *dial = new SwitchScreen(command, DIALER);
+    ActionButton *dialer   = new ActionButton(0,   0, 240, 80, dial, "Make Call", command, display);
+    //ActionButton *texter   = new ActionButton(0,  80, 240, 80, GOTO_TEXTER,    "Send Text", command, display);
+    //ActionButton *viewText = new ActionButton(0, 160, 240, 80, GOTO_VIEW_TEXT, "View Text", command, display);
+    //Register them as both drawlable and touchable
     drawable[0] = dialer;
-    drawable[1] = texter;
-    drawable[2] = viewText;
+    //drawable[1] = texter;
+    //drawable[2] = viewText;
     touchable[0] = dialer;
-    touchable[1] = texter;
-    touchable[2] = viewText;
+    //touchable[1] = texter;
+    //touchable[2] = viewText;
 }
