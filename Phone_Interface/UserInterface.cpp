aa93ad792e76bddc7e1ec5c868197b7b7cfbce39
Jonathan Wegener <jlwegene@indiana.edu>
2014-10-23 20:34:29 -0400

    ok

--- Phone_Interface/UserInterface.cpp
+++ Phone_Interface/UserInterface.cpp
@@ -17,8 +17,8 @@ void UserInterface::touch(point& p){
 
 void UserInterface::draw(){
   
-  //screen->cls();
-  screen->fill(0,0,240,340, 0);
+  // screen->cls();
+  screen->fill(0,0,240,340, 768);
   
   for(int i = 0; i < numberDrawable; i++){
      drawable[i]->draw();    
