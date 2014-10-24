0c5a0b91ebf68dbdce39100e129115d65fd3267c
Jonathan Wegener <jlwegene@indiana.edu>
2014-10-23 21:13:15 -0400

    ok

--- Phone_Interface/Dialer.cpp
+++ Phone_Interface/Dialer.cpp
@@ -2,16 +2,16 @@
 #include "Display.h"
 
 Dialer::Dialer(Command *command, SeeedStudioTFTv2 *display): UserInterface(13 ,14, 1, display){
-    CharButton *cb0 = new CharButton( 80, 270, 80, 50, "0", '0',"yz#", command, display);
+    CharButton *cb0 = new CharButton( 80, 270, 80, 50, "0", '0',"", command, display);
     CharButton *cb1 = new CharButton(  0, 120, 80, 50, "1", '0',"", command, display);
-    CharButton *cb2 = new CharButton( 80, 120, 80, 50, "2", '0',"abc", command, display);
-    CharButton *cb3 = new CharButton(160, 120, 80, 50, "3", '0',"def", command, display);
-    CharButton *cb4 = new CharButton(  0, 170, 80, 50, "4", '0',"ghi", command, display);
-    CharButton *cb5 = new CharButton( 80, 170, 80, 50, "5", '0',"jkl", command, display);
-    CharButton *cb6 = new CharButton(160, 170, 80, 50, "6", '0',"mno", command, display);
-    CharButton *cb7 = new CharButton(  0, 220, 80, 50, "7", '0',"pqr", command, display);
-    CharButton *cb8 = new CharButton( 80, 220, 80, 50, "8", '0',"stu", command, display);
-    CharButton *cb9 = new CharButton(160, 220, 80, 50, "9", '0',"vwx", command, display);
+    CharButton *cb2 = new CharButton( 80, 120, 80, 50, "2", '0',"", command, display);
+    CharButton *cb3 = new CharButton(160, 120, 80, 50, "3", '0',"", command, display);
+    CharButton *cb4 = new CharButton(  0, 170, 80, 50, "4", '0',"", command, display);
+    CharButton *cb5 = new CharButton( 80, 170, 80, 50, "5", '0',"", command, display);
+    CharButton *cb6 = new CharButton(160, 170, 80, 50, "6", '0',"", command, display);
+    CharButton *cb7 = new CharButton(  0, 220, 80, 50, "7", '0',"", command, display);
+    CharButton *cb8 = new CharButton( 80, 220, 80, 50, "8", '0',"", command, display);
+    CharButton *cb9 = new CharButton(160, 220, 80, 50, "9", '0',"", command, display);
 
     Action *backspace = new Backspace(command);
     ActionButton *backspaceButton = new ActionButton(160, 270, 80, 50, backspace, "Back", command, display);
