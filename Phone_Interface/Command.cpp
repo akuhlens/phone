39371c5cdac9e80cf0b53ede17a4c10575ef4fc1
Jonathan Wegener <jlwegene@indiana.edu>
2014-10-27 01:38:24 -0400

    hacks

--- Phone_Interface/Command.cpp
+++ Phone_Interface/Command.cpp
@@ -247,7 +247,7 @@ void NextText::envoke(){
     char number[12] = {0};
     char buffer[256] = {0};
     if(!cmd->cell->readSMS(number, buffer, *index) && *number){
-        sprintf("From %s\n\r%s"number,buffer, buff)
+        sprintf("From %s\n\r%s"number,buffer, buff);
         *index += 1;
         cmd->ui[cmd->currentUI]->draw();
     }
