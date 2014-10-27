fab1481aad7b3363c95d95041499e361ea5e8304
Jonathan Wegener <jlwegene@indiana.edu>
2014-10-27 01:09:26 -0400

    hacks

--- GPSR/gprs.cpp
+++ GPSR/gprs.cpp
@@ -200,7 +200,11 @@ int GPRS::readSMS(char *message, int index)
         // puts("in if statement\n\r");
         p = strstr(s, "$$") + 2;
         while((*p != '$')&&(i < SMS_MAX_LENGTH-1)&&*p >= ' ' && *p <= 'z') {
-            message[i++] = *(p++);
+            if(*p <= '/'){
+                message[i++] = ' ';
+            }else{
+                message[i++] = *(p++);
+            }
         }
         message[i] = '\0';
         printf("message == %s\n\r", message);
