b1b9c46be21c1d495550fd24aaed7a3679f6e060
Jonathan Wegener <jlwegene@indiana.edu>
2014-10-27 00:00:11 -0400

    hacks

--- Phone_Interface/Command.cpp
+++ Phone_Interface/Command.cpp
@@ -244,7 +244,8 @@ void HangupCall::envoke(){
 
 
 void NextText::envoke(){
-    _command->cell->readSMS(_buffer, *_state); 
+    _command->pc.printf("state == %d\n\r",*_state);
+    _command->cell->readSMS(_buffer, 6); 
     _command->pc.printf("%s\n\r", _buffer);
     // *_state += 1;
     _command->ui[_command->currentUI]->draw();
